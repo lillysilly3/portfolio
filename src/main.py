979f934ad76a5c77b2de7c1ff8fe8a3560371fa3
Paulina <paulinasukure@gmail.com>
2026-03-24 11:36:43 +0200

    add copy static

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+import shutil
+import os
+
 from textnode import TextNode, TextType
+from copystatic import copy_files_recursive
 
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
+
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+
+    copy_files_recursive("./static", "./public")
 
 main()
